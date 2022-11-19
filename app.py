b6b91555e7f9ec26b872762931a46442cb6f8669
Mason Drabik <mkdrabik@unc.edu>
2022-11-19 01:33:58 -0500

    done

--- app.py
+++ app.py
@@ -1,63 +1,37 @@
 from flask import Flask, render_template, request
-from helpers import todo
+from helpers import workout
 
 app: Flask = Flask(__name__)
-todo_list: list[todo] = []
-todo_count: int = 0
+workout_list: list[workout] = []
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/create-todo', methods=["GET", "POST"])
-def create_todo():
+@app.route('/create-workout', methods=["GET", "POST"])
+def create_workout():
     if request.method == "POST":
-        global todo_list
-        global todo_count
+        global workout_list
         
-        title: str = request.form['title']
-        description: str = request.form['description']
-        color: str = request.form['color']
+        goal: str = request.form['goal']
+        type: str = request.form['workout']
+        print(type)
 
-        if title == '':
-            return render_template("create-todo.html")
 
-        new_todo: todo = todo(todo_count, title, description, color)
-        todo_list.append(new_todo)
+        new_workout: workout = workout(goal, type)
+        workout_list.append(new_workout)
 
-        todo_count += 1
 
-        return render_template("success.html", title=title, description=description)
-    return render_template("create-todo.html")
+        return render_template("success.html", title=goal, description=type)
+    return render_template("create-workout.html")
 
 
 @app.route('/view-todo-list', methods=["GET", "POST"])
 def view_todo_list():
-    if request.method == "POST" and len(todo_list) > 0:
-        idx: int = int(request.form['check-button'])
-        todo_list[idx].checked = not todo_list[idx].checked
-    return render_template('view-list.html', todo_list=todo_list)
+   
+    return render_template('view-list.html', workout_list=workout_list)
 
-
-@app.route('/edit-todo<todo_number>', methods=["GET", "POST"])
-def edit_todo(todo_number: str):
-    if request.method == "POST":
-        global todo_list
-
-        title: str = request.form['title']
-        description: str = request.form['description']
-        color: str = request.form['color']
-
-        if title == '':
-            return render_template("edit-todo.html")
-
-        todo_list[int(todo_number)].title = title
-        todo_list[int(todo_number)].description = description
-        todo_list[int(todo_number)].color = color
-
-        return render_template("edit-success.html")
-    return render_template('edit-todo.html', todo=todo_list[int(todo_number)])
 
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -1,13 +1,32 @@
-class todo:
-    id: int
-    title: str
-    description: str
-    color: str
-    checked: bool
+class workout:
+    goal: str
+    type: str
 
-    def __init__(self, id: int, title: str, description: str, color: str):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.color = color
-        self.checked = False # Every new item begins as unchecked
+
+    def __init__(self, reps: str, body_part: str = ""):
+        rep_count: str = ""
+        if reps == "Get Stronger":
+            rep_count = "4 - 6"
+        else:
+            rep_count = "12 - 15"
+        chest: str = f"Barbell Bench Press: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Incline Dumbell Bench: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps High to Low Flies: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Pec Deck Flies Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        back: str = f"High Rows: Warm up set of 6 reps each side, 3 sets of {rep_count} reps Landmine Rows: Warm up set of 6 reps, 3 sets of {rep_count} reps JPG Pulldowns: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps T-Bar Rows: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Barbell Shrugs: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        legs: str= f"Quad Extensions: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Prone Leg Curls: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Barbell Back Squat: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Babrell Hip Thrusts: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        shoulders: str = f"Dumbell Shoulder Press: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Rear Delt Cable Flies: Warm up set of {rep_count} reps: 3 sets of {rep_count} reps Dumbell Lateral Raises: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        triceps: str = f"Skullcrushers Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Tricep Pushdowns: Warm up set of {rep_count} reps, 2 sets of {rep_count} reps, dropset of at least 15 reps Overhead Rope Tricep Extensions: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        biceps: str = f"Preacher Curls: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Cross-Body Hammer Curls: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps Incline Dumbell Curls: Warm up set of {rep_count} reps, 3 sets of {rep_count} reps" 
+        if body_part == "Chest":
+            self.type = chest
+        elif body_part == "Back":
+            self.type = back
+        elif body_part == "Legs":
+            self.type = legs
+        elif body_part == "Shoulders":
+            self.type = shoulders
+        elif body_part == "Triceps":
+            self.type = triceps
+        else:
+            self.type = biceps
+        
+        self.goal = reps
+        
